7465ffebbba50bb62f95faf5bac4fd501e0247df
Prabhat Kumar <prabhat.kumar.hisar@gmail.com>
2025-04-28 19:41:11 +0530

    adding icons and some css for fonts

---
+++ client/src/data.js
@@ -0,0 +1,9 @@
+import LightMode from './assets/icons/Light_mode.svg';
+import DarkMode from './assets/icons/Dark_mode.svg';
+import Exit from './assets/icons/Exit.svg';
+const logo={
+    light: LightMode,
+    dark: DarkMode,
+    exit:Exit,
+}
+export default logo
